ce7e3921d20ec90e3cfe06b491a825443e94c610
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-03-21 15:55:37 -0700

    add javascript and definition content

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,83 @@
 $(function() {
 /*  X X X */
-  $(".panel").click(function(){
-    $(".show").fadeToggle();
+  $(".term-a").click(function(){
+    $(".definition-a").toggle();
+  });
+
+  $(".term-b").click(function(){
+    $(".definition-b").toggle();
+  });
+
+  $(".term-c").click(function(){
+    $(".definition-c").toggle();
+  });
+
+  $(".term-d").click(function(){
+    $(".definition-d").toggle();
+  });
+
+  $(".term-e").click(function(){
+    $(".definition-e").toggle();
+  });
+
+  $(".term-f").click(function(){
+    $(".definition-f").toggle();
+  });
+
+  $(".term-g").click(function(){
+    $(".definition-g").toggle();
+  });
+
+  $(".term-h").click(function(){
+    $(".definition-h").toggle();
+  });
+
+  $(".term-i").click(function(){
+    $(".definition-i").toggle();
+  });
+
+  $(".term-j").click(function(){
+    $(".definition-j").toggle();
+  });
+
+  $(".term-k").click(function(){
+    $(".definition-k").toggle();
+  });
+
+  $(".term-l").click(function(){
+    $(".definition-l").toggle();
+  });
+
+  $(".term-m").click(function(){
+    $(".definition-m").toggle();
+  });
+
+  $(".term-n").click(function(){
+    $(".definition-n").toggle();
+  });
+
+  $(".term-o").click(function(){
+    $(".definition-o").toggle();
+  });
+
+  $(".term-p").click(function(){
+    $(".definition-p").toggle();
+  });
+
+  $(".term-q").click(function(){
+    $(".definition-q").toggle();
+  });
+
+  $(".term-r").click(function(){
+    $(".definition-r").toggle();
+  });
+
+  $(".term-s").click(function(){
+    $(".definition-s").toggle();
+  });
+
+  $(".term-t").click(function(){
+    $(".definition-t").toggle();
   });
 
 
@@ -13,21 +89,8 @@ $(function() {
 
 
 
-/*$(".clickable").click(function() {
-    $("#showing").fadeToggle();
-    $("#hidden").fadeToggle();
-  });
 
-$("button#dark").click(function() {
-    $("body").removeClass();
-    $("body").addClass("dark-theme");
-  });
 
-  $("button#light").click(function() {
-    $("body").removeClass();
-    $("body").addClass("light-theme");
-  });
-*/
 
 /*  X X X */
 });
