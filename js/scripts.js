a6d415d3a7f3c671a8d1206d9325c82615b65608
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-03-21 14:28:26 -0700

    add rows, columns, and vocabulary terms

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(function() {
+/*  X X X */
+  $(".panel").click(function(){
+    $(".show").fadeToggle();
+  });
+
+
+
+
+
+
+
+
+
+
+/*$(".clickable").click(function() {
+    $("#showing").fadeToggle();
+    $("#hidden").fadeToggle();
+  });
+
+$("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-theme");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-theme");
+  });
+*/
+
+/*  X X X */
+});
